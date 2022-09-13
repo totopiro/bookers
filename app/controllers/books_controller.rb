bb357c0f4097da66d58a697bc78534e945a9148a
EC2 Default User <ec2-user@ip-172-31-28-43.ec2.internal>
2022-09-13 18:03:34 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  def index
+    #index部分
+    @books = Book.all
+
+    #new部分
+
+    #Viewへ渡すためのインスタンス変数にからのModelオブジェクトを作成
+    @book = Book.new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all #nilによるエラーを解消するために定義
+
+    if @book.save
+      flash[:created] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+      # redirect_to "/books/indexnew"
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
